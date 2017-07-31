193e4ae88e1148271ee622bc2a0f28b83ccabc19
unknown <rosiek.kuba@gmail.com>
2017-07-31 16:17:40 +0100

    do poprawy

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+var buttons = document.getElementsByClassName('button'); // Pobiera elementy z klasy button i przypisuje do zmiennej buttons
+		for (var i = 0 ; i == buttons.length ; i++) { 	// petla, wykonuje sie tyle razy ile jest elementow button
+			buttons[1].innerText = '' 
+		};
+alert()	;		
