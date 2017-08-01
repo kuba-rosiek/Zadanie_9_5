e32bcb645a92484e9288d445e2459b356f058070
kuba-rosiek <rosiek.kuba@gmail.com>
2017-08-01 09:21:53 +0100

    Update scripts.js
    Działa

--- scripts.js
+++ scripts.js
@@ -3,5 +3,5 @@ var buttons = document.getElementsByClassName('button');
 		var i;
 		for (i = 0 ; i > buttons.length ; i++) { 	
 		buttons[i].innerText;	
+		console.log(buttons[i].innerText);
 		};	
-console.log(buttons[i].innerText);
